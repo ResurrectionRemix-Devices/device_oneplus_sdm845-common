2355b9f569e2d815a05b68d2ad3a299153ac9014
Anirudh Gupta <anirudhgupta109@gmail.com>
2018-10-02 12:08:21 +0000

    {KeyHandler,doze,pocketmode}: Enable LOCAL_PRIVATE_PLATFORM_APIS
    Signed-off-by: Anirudh Gupta <anirudhgupta109@gmail.com>

--- KeyHandler/Android.mk
+++ KeyHandler/Android.mk
@@ -20,7 +20,7 @@ include $(CLEAR_VARS)
 
 LOCAL_SRC_FILES := $(call all-java-files-under, src)
 LOCAL_PACKAGE_NAME := KeyHandler
-
+LOCAL_PRIVATE_PLATFORM_APIS := true
 LOCAL_CERTIFICATE := platform
 LOCAL_PRIVILEGED_MODULE := true
 LOCAL_MODULE_TAGS := optional
--- pocketmode/Android.mk
+++ pocketmode/Android.mk
@@ -7,11 +7,12 @@ LOCAL_MODULE_TAGS := optional
 LOCAL_SRC_FILES := $(call all-java-files-under, src)
 
 LOCAL_PACKAGE_NAME := OnePlusPocketMode
+LOCAL_PRIVATE_PLATFORM_APIS := true
 LOCAL_CERTIFICATE := platform
 LOCAL_PRIVILEGED_MODULE := true
 
-LOCAL_STATIC_JAVA_LIBRARIES := \
-    org.lineageos.platform.internal
+#LOCAL_STATIC_JAVA_LIBRARIES := \
+#    org.lineageos.platform.internal
 
 LOCAL_PROGUARD_FLAG_FILES := proguard.flags
 
