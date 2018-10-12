7244f359721093c20faf79925968564c10f46c34
Anirudh Gupta <anirudhgupta109@gmail.com>
2018-10-12 15:19:30 +0000

    pocketmode: Change util to AOSiP's
    Signed-off-by: Anirudh Gupta <anirudhgupta109@gmail.com>

--- pocketmode/src/com/cyanogenmod/pocketmode/PocketSensor.java
+++ pocketmode/src/com/cyanogenmod/pocketmode/PocketSensor.java
@@ -25,7 +25,7 @@ import android.hardware.SensorManager;
 import android.text.TextUtils;
 import android.util.Log;
 
-import com.android.internal.util.liquid.FileUtils;
+import com.android.internal.util.aosip.aosipUtils;
 
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
